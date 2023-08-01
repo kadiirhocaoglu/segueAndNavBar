9f81ab95eeadf0e44188db66562a79319f58a634
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2023-08-01 17:46:46 +0300

    add perform segue and switch controller

--- segueAndNavBar/SegueViewController.swift
+++ segueAndNavBar/SegueViewController.swift
@@ -9,15 +9,22 @@ import UIKit
 
 class SegueViewController: UIViewController {
     @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var segueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         segue.destination.navigationItem.title = titleTextField.text
+    }
+    @IBAction func buttonTapped (_ button : UIButton)
+    {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "nextSegue", sender: nil)
+        }
     }
 
 }
